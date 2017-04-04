7cb4b231816244be48658b28f2f758fd7c4bc7e8
Jonathan DeMasi <jrdemasi@gmail.com>
2017-04-04 09:44:18 -0600

    Update returnlinks.py

--- returnlinks.py
+++ returnlinks.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+"Crawls" a website and returns all of the links that appear on the site.
+Can be modified for other protocols, but the below covers http, https,
+and ftp.
+"""
+
+
 from urllib.request import urlopen
 import re
 import sys
