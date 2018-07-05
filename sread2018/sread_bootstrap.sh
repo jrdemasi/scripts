c8f2f34066c65665fdb7a9d01445b0b2fc998ac1
MHG <hynesgra@colorado.edu>
2018-07-05 15:02:45 -0600

    file names

--- sread2018/sread_bootstrap.sh
+++ sread2018/sread_bootstrap.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # A few housekeeping items:
-timedatectl set-timezone America/Denver
 curl -o /var/tmp/gh_create.py https://raw.githubusercontent.com/jrdemasi/scripts/master/sread2018/gh_create.py
 chmod +x /var/tmp/gh_create.py
 
 # Add users as args to following: 
-/var/tmp/gh_create.py -d /Users -s /scratch/Users https://raw.githubusercontent.com/jrdemasi/scripts/master/sread2018/sread-users.txt
+/var/tmp/gh_create.py -d /Users -s /scratch/Users https://raw.githubusercontent.com/jrdemasi/scripts/master/sread2018/shortread-users.txt
 
 # Create a cronjob to run gh_create.py every 5 minutes:
-echo "*/5 * * * * root /var/tmp/gh_create.py -d /Users -s /scratch/Users https://raw.githubusercontent.com/jrdemasi/scripts/master/sread2018/sread-users.txt" > /etc/cron.d/gh_create
+echo "*/5 * * * * root /var/tmp/gh_create.py -d /Users -s /scratch/Users https://raw.githubusercontent.com/jrdemasi/scripts/master/sread2018/shortread-users.txt" > /etc/cron.d/gh_create
 
